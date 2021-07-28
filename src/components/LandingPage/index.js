3c37b9ed5445f0c62cc08d378be68912fe695b60
Joseph Amador <kirito.1986.714@gmail.com>
2021-07-27 17:09:39 -0700

    added my components,and utils directories

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Test LandingPage!!!!</h1>
+        </div>
+    )
+}
+
+export default LandingPage
